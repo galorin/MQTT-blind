71c2cdf60e1f045f5a66d6e4a47ecc8947b2c694
Sarah <galorin@gmail.com>
2019-06-19 11:41:49 +0100

    Add commented functions
    Added reset and run to stop functions.

--- MQTT-blind.ino
+++ MQTT-blind.ino
@@ -1,3 +1,4 @@
+
 #include <AccelStepper.h>
 
 #define dirPin D1
@@ -6,12 +7,37 @@
 #define upButton D4 // Button to manually move blind up.  
 #define downButton D5 // Button to manually move blind up.  
 
+#define stepsToClose 10000 // number of steps from fully opened to fully closed.
+
 // The buttons should work as follows
 //
 // Button click moves blind up/down
 // A second click while blind is in motion will stop the blind at that point
+// If the end stop is hit before the open command is finished, halt and set 
+//   current position to 0
+
+int currentPosition = 0;
+int millisPerStep = 50;
 
 AccelStepper blind(1,stepPin,dirPin);
+
+void resetToEndStop() {
+	// Check if Endstop is triggerd.
+	//  If currently triggered, set current position to 0. Else
+	// Lower the blind by 5cm, then execute runToStop().
+}
+
+void runToStop() {
+	// Movement loop
+	//-------------------------------------
+	// Move blind a certain number of steps
+    // check endstop
+	//  if endtop triggered, 
+	//    stop loop
+	//    set current position to 0
+	// end function
+	//-------------------------------------
+}
 
 void setup() {
   // put your setup code here, to run once:
