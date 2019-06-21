823905c9ea63f582a370906f8fde20865196152c
Sarah <galorin@gmail.com>
2019-06-21 17:32:33 +0100

    Will it work... who knows
    Code changes that may work

--- MQTT-blind.ino
+++ MQTT-blind.ino
@@ -16,10 +16,52 @@
 // If the end stop is hit before the open command is finished, halt and set 
 //   current position to 0
 
-int currentPosition = 0;
-int millisPerStep = 50;
+int millisPerStep = 20;
+
+enum movementEnum {
+	MovingDown.
+	Stopped,
+	MovingUp
+};
+
+movementEnum move = Stopped;
 
 AccelStepper blind(1,stepPin,dirPin);
+
+void setup() {
+  // put your setup code here, to run once:
+  blind.setMaxSpeed(1000);
+  // initialize input pins.
+  pinMode(topEnd, INPUT_PULLUP);
+  pinMode(upButton, INPUT_PULLUP);
+  pinMode(downButton, INPUT_PULLUP);
+  attachInterrupt(digitalPinToInterrupt(topEnd), endStopHit, RISING);
+  attachInterrupt(digitalPinToInterrupt(upButton), openCall, RISING);
+  attachInterrupt(digitalPinToInterrupt(downButton), closeCall, RISING);
+  resetToEndStop();
+  blind.setSpeed(800);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  
+  // while moving up, move up by one step until 
+  if (movingUp && !movingDown) {
+	  if (blind.currentPosition() >=0)
+	  blind.moveTo(blind.currentPosition() -10);
+  }
+  if (movingDown && !movingUp) {
+	  
+  }
+}
+
+void endStopHit()
+{
+	blind.stop();
+	blind.setCurrentPosition = 0;
+	movingUp = false;
+	movingDown = false;
+}
 
 void resetToEndStop() {
 	// Check if Endstop is triggerd.
@@ -39,14 +81,14 @@ void runToStop() {
 	//-------------------------------------
 }
 
-void setup() {
-  // put your setup code here, to run once:
-  blind.setMaxSpeed(1000);
-  // initialize input pins.
+// If currently closing, stop and reverse direction.  
+// If opening, stop.
+// If not moving, start opening
+void openCall() {
+	movingDown = false;
+	movingUp = !movingUp;
 }
 
-void loop() {
-  // put your main code here, to run repeatedly:
-blind.setSpeed(400);
-blind.runSpeed();
+void closeCall() {
+	
 }
